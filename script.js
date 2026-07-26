459792f057872e778bc4e55363b483a3125a9653
Wesley.J <westwesley2022gmail.com>
2026-07-26 20:55:33 +0300

    Initialize the variables

---
+++ script.js
@@ -0,0 +1,4 @@
+//create array for female, male and days of the week
+const malesNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
